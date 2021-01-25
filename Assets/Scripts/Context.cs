211516990be2ab2713d5407dd2bf24d1fba57f1b
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-25 10:57:49 -0600

    adds Singleton and StoryNode classes

---
+++ Assets/Scripts/Context.cs
@@ -0,0 +1,9 @@
+﻿public class Context : Singleton<Context>
+{
+    public bool gameStarted = false;
+    
+    private StoryNode start;
+    
+    public string[] bountyClues;
+    public Choice[] scannerData;
+}
