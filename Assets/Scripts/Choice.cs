cf6ce20ebab3e0525a323380fdaad89b96fe04f5
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-25 11:28:46 -0600

    adds StoryNode and Choice scriptable objects

---
+++ Assets/Scripts/Choice.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/ChoiceScriptableObject", order = 2)]
+public class Choice : ScriptableObject
+{
+    public string command;
+    public string description;
+    public bool hasDialog;
+    public string dialog;
+    public bool isWaypoint;
+    public StoryNode nextNode;
+}
---
+++ Assets/Scripts/StoryNode.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/StoryNodeScriptableObject", order = 1)]
+public class StoryNode : ScriptableObject
+{
+    public string text;
+    public Choice[] choices;
+}
