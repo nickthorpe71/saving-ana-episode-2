902aece6a194e3323312a8627b8ab3e5c9d59cb3
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-25 17:07:11 -0600

    adds all Misc StoryNodes

--- Assets/Scripts/Context.cs
+++ Assets/Scripts/Context.cs
@@ -1,25 +1,30 @@
-﻿public class Context : Singleton<Context>
+﻿using System.Collections.Generic;
+
+public class Context : Singleton<Context>
 {
     public bool gameStarted = false;
     
     private StoryNode start;
     
-    public Dictionary<string, string> clues = new Dictionary<string, string>(){
+    public Dictionary<string, string> clues = new Dictionary<string, string>()
+    {
         {"Topps", "clue about bounty"},
         {"Zella", "clue about bounty"},
         {"Anigun", "clue about bounty"},
     };
 
 
-    public Dictionary<string, string> scannerData = new Dictionary<string, string>(){
-        {"Topps", new Character {name="Glenn (Uno) Topps", description="Wears a ragged brown cloak which covers his face. All that portrudes from the hood is a lit cigarette and a red glow from what looks to be where one of his eyes might be.", occupation="N/A"}},
-        {"Zella", new Character {name="Zella Stingwray", description="", occupation="CEO of IronNori - the largest mining company in the galaxy"}},
-        {"Anigun", new Character {name="Anigun", description="", occupation="Bartender"}},
+    public Dictionary<string, Character> scannerData = new Dictionary<string, Character>()
+    {
+        {"Topps", new Character { name="Glenn (Uno) Topps", description="Wears a ragged brown cloak which covers his face. All that portrudes from the hood is a lit cigarette and a red glow from what looks to be where one of his eyes might be.", occupation="N/A"} },
+        {"Zella", new Character { name="Zella Stingwray", description="", occupation="CEO of IronNori - the largest mining company in the galaxy"} },
+        {"Anigun", new Character { name="Anigun", description="", occupation="Bartender"} },
     };
 }
 
-public class Character{
-    public string name { get; set; };
-    public string description { get; set; };
-    public string occupation { get; set; };
-}
+public class Character
+{
+    public string name { get; set; }
+    public string description { get; set; }
+    public string occupation { get; set; }
+};
