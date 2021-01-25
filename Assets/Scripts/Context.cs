c97f8efe0283504082738c246a1d40bfb45dce24
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-25 14:32:20 -0600

    adds basics to context

--- Assets/Scripts/Context.cs
+++ Assets/Scripts/Context.cs
@@ -4,6 +4,22 @@
     
     private StoryNode start;
     
-    public string[] bountyClues;
-    public Choice[] scannerData;
+    public Dictionary<string, string> clues = new Dictionary<string, string>(){
+        {"Topps", "clue about bounty"},
+        {"Zella", "clue about bounty"},
+        {"Anigun", "clue about bounty"},
+    };
+
+
+    public Dictionary<string, string> scannerData = new Dictionary<string, string>(){
+        {"Topps", new Character {name="Glenn (Uno) Topps", description="Wears a ragged brown cloak which covers his face. All that portrudes from the hood is a lit cigarette and a red glow from what looks to be where one of his eyes might be.", occupation="N/A"}},
+        {"Zella", new Character {name="Zella Stingwray", description="", occupation="CEO of IronNori - the largest mining company in the galaxy"}},
+        {"Anigun", new Character {name="Anigun", description="", occupation="Bartender"}},
+    };
+}
+
+public class Character{
+    public string name { get; set; };
+    public string description { get; set; };
+    public string occupation { get; set; };
 }
