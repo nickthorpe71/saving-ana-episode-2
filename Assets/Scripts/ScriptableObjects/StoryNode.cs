f3c5028a99322ba95b78a3d9fe8cc189b1f8411c
nickthorpe71 <nickthorpe71@gmail.com>
2021-01-30 07:49:54 -0600

    adds new story scene

--- Assets/Scripts/ScriptableObjects/StoryNode.cs
+++ Assets/Scripts/ScriptableObjects/StoryNode.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.UI
 
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/StoryNodeScriptableObject", order = 1)]
 public class StoryNode : ScriptableObject
 {
     public string text;
+    public Image image;
     public Choice[] choices;
 }
