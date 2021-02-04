f6f973cf3df73d1605c6c96d1d2bfe26aa87361a
nickthorpe71 <nickthorpe71@gmail.com>
2021-02-04 07:29:31 -0600

    adds gravity to planets

--- Assets/Scripts/Travel/Asteriod.cs
+++ Assets/Scripts/Travel/Asteriod.cs
@@ -15,30 +15,52 @@ public class Asteriod : MonoBehaviour
     {
         trans = transform;
         rotationOffset = Random.Range(2f, 10f);
-        scaleIncrease = Random.Range(3f, 20f);
     }
 
     void Start()
     {
+        SetScale();
+
+        randomRotation.x = Random.Range(-rotationOffset, rotationOffset);
+        randomRotation.y = Random.Range(-rotationOffset, rotationOffset);
+        randomRotation.z = Random.Range(-rotationOffset, rotationOffset);
+
+        GetComponent<Rigidbody>().mass = Mathf.Pow(scaleIncrease, 4);
+        GetComponent<Rigidbody>().AddForce(Random.Range(-12 - scaleIncrease, 12 + scaleIncrease), Random.Range(0, 12 + scaleIncrease) * -1, 0, ForceMode.Impulse);
+
+    }
+
+    void SetScale()
+    {
+        int chance = Random.Range(1, 100);
+
+        scaleIncrease = Random.Range(3f, 4.4f);
+
+        if (chance > 10 && chance < 86)
+            scaleIncrease += Random.Range(3f, 9.8f);
+
+        else if (chance > 85)
+            scaleIncrease *= scaleIncrease;
+        
         Vector3 scale = Vector3.one;
 
         scale.x = Random.Range(scaleIncrease - scaleOffset, scaleIncrease + scaleOffset);
         scale.y = Random.Range(scaleIncrease - scaleOffset, scaleIncrease + scaleOffset);
         scale.z = Random.Range(scaleIncrease - scaleOffset, scaleIncrease + scaleOffset);
         trans.localScale = scale;
-
-        randomRotation.x = Random.Range(-rotationOffset, rotationOffset);
-        randomRotation.y = Random.Range(-rotationOffset, rotationOffset);
-        randomRotation.z = Random.Range(-rotationOffset, rotationOffset);
-
-        GetComponent<Rigidbody>().mass = scaleIncrease * 3;
-        GetComponent<Rigidbody>().AddForce(Random.Range(-12 - scaleIncrease, 12 + scaleIncrease), Random.Range(0, 12 + scaleIncrease) * -1, 0, ForceMode.Impulse);
-
     }
 
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Player")
+        {
+            Destroy(collider.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collider)
+    {
+        if (collider.gameObject.tag == "Asteroid")
         {
             Destroy(collider.gameObject);
         }
---
+++ Assets/Scripts/Travel/Attractor.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+
+    [HideInInspector] public Rigidbody rb;
+
+    const float G = 0.00006674f;
+
+    public static List<Attractor> Attractors;
+
+    void OnEnable()
+    {
+        if (Attractors == null)
+            Attractors = new List<Attractor>();
+        Attractors.Add(this);
+    }
+
+    void OnDisable()
+    {
+        Attractors.Remove(this);
+    }
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        foreach (Attractor attractor in Attractors)
+        {
+            if (attractor != this)
+                Attract(attractor);
+        }
+    }
+
+    void Attract(Attractor objToAttract)
+    {
+        Rigidbody rbToAttract = objToAttract.rb;
+
+        Vector3 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        if (distance == 0f)
+            return;
+
+        // Newton's central equation of universal gravitation
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+    }
+}
