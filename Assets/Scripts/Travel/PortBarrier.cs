18374a6276e3c4597a2e1e3691595a395cf2a7df
nickthorpe71 <nickthorpe71@gmail.com>
2021-02-07 11:40:16 -0600

    adds start and end ports

---
+++ Assets/Scripts/Travel/PortBarrier.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortBarrier : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Destroy(collider.gameObject);
+        }
+    }
+}
