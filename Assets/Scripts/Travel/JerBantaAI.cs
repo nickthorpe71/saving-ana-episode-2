5d1eea7412bf42f387326112d2f2aa844836d65f
nickthorpe71 <nickthorpe71@gmail.com>
2021-02-10 07:37:35 -0600

    adds control script for jer banta

--- Assets/Scripts/Travel/JerBantaAI.cs
+++ Assets/Scripts/Travel/JerBantaAI.cs
@@ -4,15 +4,92 @@ using UnityEngine;
 
 public class JerBantaAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [HideInInspector] public float movementSpeed = 15f;
+    private float turnSpeed = 150f;
+
+    private Transform trans;
+
+    public Transform model;
+
+    private Thrusters thrusters;
+
+    public GameObject explosionPrefab;
+
+    void Awake()
     {
-        
+        trans = transform;
+        thrusters = GetComponent<Thrusters>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        StartCoroutine(ChangeSpeed());
+    }
+
     void Update()
     {
-        
+        Thrust();
+        Turn();
+        CheckThrusters();
+        ScreenConstraints();
+
+    }
+
+    void ScreenConstraints()
+    {
+        if (trans.position.x < -24.5f)
+        {
+            trans.position = new Vector3(24.2f, trans.position.y, trans.position.z);
+            thrusters.ResetTrails();
+        }
+        else if (trans.position.x > 24.5f)
+        {
+            trans.position = new Vector3(-24.2f, trans.position.y, trans.position.z);
+            thrusters.ResetTrails();
+        }
+    }
+
+    void CheckThrusters()
+    {
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            thrusters.Accelerate();
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            thrusters.Reverse();
+        }
+        else if (Input.GetAxis("Vertical") == 0)
+        {
+            thrusters.Stop();
+        }
+        else if (Input.GetAxis("Horizontal") != 0)
+        {
+            thrusters.Accelerate();
+        }
+    }
+
+    void Turn()
+    {
+        float roll = turnSpeed * Time.deltaTime * Mathf.Abs(trans.position.x / 5) * Input.GetAxis("Horizontal");
+        model.Rotate(0, 0, -roll);
+        trans.position += trans.right * movementSpeed * 1.5f * Time.deltaTime * Input.GetAxis("Horizontal");
+    }
+
+    void Thrust()
+    {
+        trans.position += trans.forward * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+    }
+
+    public void Explode()
+    {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    IEnumerator ChangeSpeed()
+    {
+        yield return new WaitForSeconds(Random.Range(0.25f, 4f));
+        movementSpeed += Random.Range(-3f, 3f);
     }
 }
