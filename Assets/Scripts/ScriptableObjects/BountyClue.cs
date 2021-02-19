1b95a9750bf007914e7f2bf31d14afeb6e344183
nickthorpe71 <nickthorpe71@gmail.com>
2021-02-19 05:54:39 -0600

    adds Bounty Clue SO

---
+++ Assets/Scripts/ScriptableObjects/BountyClue.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/StoryNodeScriptableObject", order = 1)]
+public class BountyClue : ScriptableObject
+{
+    [TextArea(15, 20)]
+    public string text;
+}
