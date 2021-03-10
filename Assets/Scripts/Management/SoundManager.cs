513c99e4e999610f10650d666c913c3ccb91d478
nickthorpe71 <nickthorpe71@gmail.com>
2021-03-10 07:03:12 -0600

    adds music manager check for specific scenes

--- Assets/Scripts/Management/SoundManager.cs
+++ Assets/Scripts/Management/SoundManager.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour
 {
 	public AudioSource MusicSource;
 	public static SoundManager Instance = null;
+
+    public AudioClip mainMusic;
+    public AudioClip storyMusic;
+    public AudioClip travelMusic;
+
+    private int currentLevel = 5555;
 
 	private void Awake()
 	{
@@ -19,6 +26,38 @@ public class SoundManager : MonoBehaviour
 		}
 		DontDestroyOnLoad (gameObject);
 	}
+
+    private void Start() 
+    {
+        CheckSceneMusic();
+    }
+
+    private void OnLevelWasLoaded(int level) 
+    {
+        Debug.Log("hit");
+
+        if (level != currentLevel)
+        {
+            currentLevel = level;
+            CheckSceneMusic();
+        }
+    }
+
+    private void CheckSceneMusic()
+    {
+        switch (SceneManager.GetActiveScene().name)
+            {
+                case "Main":
+                    PlayMusic(mainMusic);
+                    break;
+                case "Story":
+                    PlayMusic(storyMusic);
+                    break;
+                case "Travel Short":
+                    PlayMusic(travelMusic);
+                    break;
+            }
+    }
 
 	public void PlayMusic(AudioClip clip)
 	{
