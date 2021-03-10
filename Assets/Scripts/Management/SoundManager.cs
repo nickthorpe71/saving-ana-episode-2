b99891effc27841d863a081887e74bc73a8fa67b
nickthorpe71 <nickthorpe71@gmail.com>
2021-03-10 06:40:50 -0600

    adds music manager

---
+++ Assets/Scripts/Management/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+	public AudioSource MusicSource;
+	public static SoundManager Instance = null;
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else if (Instance != this)
+		{
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad (gameObject);
+	}
+
+	public void PlayMusic(AudioClip clip)
+	{
+		MusicSource.clip = clip;
+		MusicSource.Play();
+	}
+}
